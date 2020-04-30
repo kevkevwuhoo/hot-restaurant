97180f550c62f1a9608ca5a6b659843f6bbd4199
kwu3 <kwu3@villanova.edu>
2020-04-30 14:01:17 -0400

    Added initial reservation and waitlist data and their routes. changed to utilize es6

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8000;
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// store reservation & waitlist data
+let reservations = [
+	{
+		customerName: "Kevin",
+		customerEmail: "kevkevwu@example.com",
+		customerID: "kevkevwu",
+		phoneNumber: "000-000-0000",
+	},
+];
+let waitlist = [];
+
+// routes
+app.get("/api/tables", (req, res) => {
+	return res.json(reservations);
+});
+
+app.get("/api/waitlist", (req, res) => {
+	return res.json(waitlist);
+});
+
+app.listen(PORT, function () {
+	console.log("App listening on PORT " + PORT);
+});
